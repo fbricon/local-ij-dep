1e33d64ef4f398bb8d6e8fbf9fbf95dba1e0422e
GitHub Action <action@github.com>
2023-12-05 17:06:57 +0000

    Template cleanup

--- src/main/kotlin/com/github/fbricon/localijdep/services/MyProjectService.kt
+++ src/main/kotlin/com/github/fbricon/localijdep/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.fbricon.localijdep.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.fbricon.localijdep.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
